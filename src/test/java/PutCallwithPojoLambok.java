51ceaa538af1041163bf1cb8217f608673a9e185
Anup kapale <anup.kapale@gmail.com>
2023-08-05 13:56:58 +0530

    Put call added with pojo lambok

--- src/test/java/PutCallwithPojoLambok.java
+++ src/test/java/PutCallwithPojoLambok.java
@@ -12,14 +12,15 @@ import org.testng.annotations.Test;
 
 import java.io.IOException;
 
-public class PostCallwithPojoLombak {
+public class PutCallwithPojoLambok {
 
     static Playwright playwright;
     static APIRequest apiRequest;
     static APIRequestContext apiRequestContext;
-    static APIResponse apiResponse;
+    //static APIResponse apiResponse;
 
     static String emailid;
+    static String userId;
 
     @BeforeTest
     public void setUp ( ) {
@@ -39,49 +40,74 @@ public class PostCallwithPojoLombak {
 
         //Create user data from pojo classes with lombk method
         UsersLambok userData =  UsersLambok.builder ()
-                 .name ( "Tenali Ramakrishna")
-                 .email ( getRandomEmail () )
-                 .gender ( "male" )
-                 .status ( "active" ).build ();
+                .name ( "Tenali Ramakrishna")
+                .email ( getRandomEmail () )
+                .gender ( "male" )
+                .status ( "active" ).build ();
 
 
-        //Initiate the post call with valid headers
-        apiResponse = apiRequestContext.post ( "https://gorest.co.in/public/v2/users" ,
+        //Initiate the post call with valid headers and data
+        APIResponse  postApiResponse = apiRequestContext.post ( "https://gorest.co.in/public/v2/users" ,
                 RequestOptions.create ( )
                         .setHeader ( "Content-Type" , "application/json" )
                         .setHeader ( "Authorization" , "Bearer 213296d3a224c4c4669fd1f4cbe3254ff92826cc45c3fdedaadb0e9754804442" )
                         .setData ( userData ) );
 
         //check the status and assert the data
-        int statusCode = apiResponse.status ( );
+        int statusCode = postApiResponse.status ( );
         Assert.assertEquals ( statusCode , 201 );
 
         System.out.println ("User Created successfully!!" );
 
-        // Covert pojo to text/json : Deserialization concept of java
-        String responseText = apiResponse.text ();
+        // Covert pojo to text
+        String responseText = postApiResponse.text ();
         ObjectMapper objectMapper = new ObjectMapper (  );
-        UsersLambok actualUser = objectMapper.readValue ( responseText, UsersLambok.class );
+        User actualUser = objectMapper.readValue ( responseText, User.class );
         Assert.assertEquals ( actualUser.getEmail (), userData.getEmail () );
         Assert.assertEquals ( actualUser.getName (), userData.getName () );
         Assert.assertEquals ( actualUser.getStatus (), userData.getStatus ());
 
         //Get the user id from response and send it to get api user
-        String  userId = actualUser.getId ();
+        userId = actualUser.getId ();
+
+        // Update the user data with some other values
+        userData.setStatus ( "inactive" );
+
+
+        // PUT CALL
+        APIResponse putApiResponse = apiRequestContext.put ( "https://gorest.co.in/public/v2/users/"+ userId ,
+                RequestOptions.create ( )
+                        .setHeader ( "Content-Type" , "application/json" )
+                        .setHeader ( "Authorization" , "Bearer 213296d3a224c4c4669fd1f4cbe3254ff92826cc45c3fdedaadb0e9754804442" )
+                        .setData ( userData ) );
+
+
+        //check the status and assert the data
+        int putApistatusCode = putApiResponse.status ( );
+        Assert.assertEquals ( putApistatusCode , 200 );
+
+        String putResponseText = putApiResponse.text ();
+        UsersLambok actualputUser = objectMapper.readValue ( putResponseText, UsersLambok.class );
+        Assert.assertEquals (actualputUser.getId (), userId  );
+        Assert.assertEquals ( actualputUser.getStatus (), userData.getStatus ());
+        System.out.println ("User Updated successfully!!" );
+
 
         //Fetch the data though get api
 
-        apiResponse = apiRequestContext.get ( "https://gorest.co.in/public/v2/users/" + userId ,
+        APIResponse getApiResponse = apiRequestContext.get ( "https://gorest.co.in/public/v2/users/" + userId ,
                 RequestOptions.create ( )
                         .setHeader ( "Authorization" , "Bearer 213296d3a224c4c4669fd1f4cbe3254ff92826cc45c3fdedaadb0e9754804442" ) );
 
 
-        int getstatusCode = apiResponse.status ( );
+        int getstatusCode = getApiResponse.status ( );
         System.out.println ( "Response status code is :" + getstatusCode );
+
+        UsersLambok actuagetlUser = objectMapper.readValue ( responseText, UsersLambok.class );
         Assert.assertEquals ( getstatusCode , 200 );
-        Assert.assertEquals (actualUser.getId (), userId  );
-        Assert.assertEquals (actualUser.getName (), userData.getName ()  );
-        Assert.assertNotNull ( actualUser.getId () );
+        Assert.assertEquals (actuagetlUser.getId (), userId  );
+        Assert.assertEquals (actuagetlUser.getName (), userData.getName ()  );
+        Assert.assertNotNull ( actuagetlUser.getId () );
 
         System.out.println ("User Fetched successfully!!" );
 
